a1eb539434ac7968329657d9a094ebd6e43591a1
Ali Abbasi Alaei <aliabbasi806@gmail.com>
2025-07-27 12:21:03 -0400

    Add install_etcd.sh script

---
+++ ste/scripts/install_etcd.sh
@@ -0,0 +1,20 @@
+etcd --version && echo "etcd is already installed" && exit 0
+
+ARCH=$(uname -m)
+
+# Only support two architectures
+if [ "$ARCH" = "x86_64" ]; then
+    ARCH="amd64"
+else
+    ARCH="arm64"
+fi
+
+wget -q --show-progress --https-only --timestamping \
+"https://github.com/coreos/etcd/releases/download/v3.5.21/etcd-v3.5.21-linux-${ARCH}.tar.gz"
+
+tar -xvf etcd-v3.5.21-linux-${ARCH}.tar.gz
+
+mv etcd-v3.5.21-linux-${ARCH}/etcd* /usr/bin/ 2>/dev/null
+
+# Verify the installation
+etcd --version
