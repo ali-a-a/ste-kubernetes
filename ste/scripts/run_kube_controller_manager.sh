50db32af30d0ef6f1e491b8659bcbe6db83f82c1
Ali Abbasi Alaei <aliabbasi806@gmail.com>
2025-07-28 09:03:15 -0400

    Add run_kube_controller_manager.sh script

---
+++ ste/scripts/run_kube_controller_manager.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Build the kube controller manager using this command inside the ali-a-a/ste-kubernetes.git repository.
+# go build -o /etc/ste-kubernetes/bin/kube-controller-manager \
+#  -ldflags="-X k8s.io/component-base/version.gitVersion=v1.32.0" ./cmd/kube-controller-manager
+
+# If the session exists, exit
+if tmux has-session -t kube-controller-manager 2>/dev/null; then
+  echo "Session kube-controller-manager exists. Exiting..."
+  exit 0
+fi
+
+# Create the tmux session
+tmux new -s kube-controller-manager
+
+# Run the kube controller manager
+tmux send-keys -t kube-controller-manager "/etc/ste-kubernetes/bin/kube-controller-manager \
+--authentication-kubeconfig=/etc/ste-kubernetes/.kube/kube-controller-manager.kubeconfig \
+--authorization-kubeconfig=/etc/ste-kubernetes/.kube/kube-controller-manager.kubeconfig \
+--bind-address=127.0.0.1 \
+--client-ca-file=/etc/ste-kubernetes/pki/ca.crt \
+--cluster-name=ste-kubernetes \
+--cluster-signing-cert-file=/etc/ste-kubernetes/pki/ca.crt \
+--cluster-signing-key-file=/etc/ste-kubernetes/pki/ca.key \
+--controllers=*,bootstrapsigner,tokencleaner \
+--kubeconfig=/etc/ste-kubernetes/.kube/kube-controller-manager.kubeconfig \
+--leader-elect=false \
+--requestheader-client-ca-file=/etc/ste-kubernetes/pki/ca.crt \
+--root-ca-file=/etc/ste-kubernetes/pki/ca.crt \
+--service-account-private-key-file=/etc/ste-kubernetes/pki/service-account.key \
+--use-service-account-credentials=true" ENTER
